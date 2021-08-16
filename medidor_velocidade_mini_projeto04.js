8e395daca9638810883ab0f09f9f97c28194d8bf
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-08-15 23:00:56 -0300

    15-08-2021 23:00

---
+++ medidor_velocidade_mini_projeto04.js
@@ -0,0 +1,9 @@
+//Velocidade maxima de 70km
+// a cada 5km acima do limite voce ganha 1 ponto
+// Math.Floor()
+// caso pontos maior que 12 -> "Carteira Suspendida"
+verificaVelocidade(70);
+
+function verificaVelocidade(velocidade) {
+    
+}
